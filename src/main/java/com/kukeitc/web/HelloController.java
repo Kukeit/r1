94d6d0b3556f18ff5ae83dff4692ca21d8d0af6f
kukeitc <kukeit.c@gmail.com>
2016-01-26 15:32:17 +0700

    2016/01/26: create 			- resources(css,js,images,fonts) 			- configs spring component scan 			- create controller and view login page

--- src/main/java/com/kukeitc/web/HelloController.java
+++ src/main/java/com/kukeitc/web/HelloController.java
@@ -1,5 +1,16 @@
 package com.kukeitc.web;
 
+import org.junit.runner.Request;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/login")
 public class HelloController {
 
+	@RequestMapping(method=RequestMethod.GET)
+	public String index(){
+		return "login_view";
+	}
 }
